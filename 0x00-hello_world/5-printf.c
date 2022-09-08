08cf121307f40e7889351d74157d2a2d06759516
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-08 16:50:37 -0700

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Execution starts here
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print("exactly with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
