43d649e867cee8c093b6ef1ac341578791df1b41
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-08 16:44:56 -0700

    Added comments to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - The entry point of the program
+ * main - Entry point
  * 
  * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
