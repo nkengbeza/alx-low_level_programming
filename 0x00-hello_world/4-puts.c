71230088f3a6e62c135497f75714b44e410c89ce
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-08 16:37:34 -0700

    Added comments to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * Simple program to print a string
- *
- * Returns 0 
+ * The entry point of the program
+ * 
+ * Description: Simple program to print a string
  */
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
