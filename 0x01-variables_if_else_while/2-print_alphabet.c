2214b640819fb8368e5346ef1506973198e20acb
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-11 06:38:50 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int n;
-	char[26] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	char[26] alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 	for (int i = 0; i < 26; i++)
 	{
