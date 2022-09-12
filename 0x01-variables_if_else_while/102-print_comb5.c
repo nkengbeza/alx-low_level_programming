053685e351a21c154f6554843e8f92a2ea28ea62
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-12 14:42:21 -0700

    Added 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -34,13 +34,13 @@ int main(void)
 						putchar(' ');
 						putchar(e);
 						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-						{
-							break;
-						}
-						putchar(',');
-						putchar(' ');
 					}
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
 					i++;
 				}
 				e++;
