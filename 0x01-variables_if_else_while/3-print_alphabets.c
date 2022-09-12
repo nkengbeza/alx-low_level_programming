7c0290e540acea2ac6b8dc02bc9123a9d496bb00
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-12 04:50:43 -0700

    Added 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 /**
   * main - Entry point of the program
   *
@@ -10,19 +9,11 @@
 int main(void)
 {
 	int i;
-	char c, alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	char c, alphabet[52] = "abcdefghijklmnopqrstuvwxyz";
 
 	for (i = 0; i < 52; i++)
-	{
-		if (i >= 26)
-		{
-			c = toupper(alphabet[i - 26]);
-		}
-		else
-		{
-			c = alphabet[i];
-		}
-		putchar(c);
+	{	
+		putchar(alphabet[i]);
 	}
 	putchar('\n');
 	return (0);
