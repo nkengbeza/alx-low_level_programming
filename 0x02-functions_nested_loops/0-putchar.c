d6383249e26e28d70d28b25f2582b8641c749caa
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-13 06:52:22 -0700

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  *
+  * Description: This application prints _putchar, followed by a new line."
+  *
+  * Return: 0. The program always successfully exits with 0
+  */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
