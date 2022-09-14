a6ebf7a3a9bcf65bdd9aed5046ea937675df950b
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-14 22:23:04 +0100

    Added 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,15 @@
   * is an alphabetic character
   *
   * Return: 1 if is alpha, otherwise 0
- */
+  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
-
+	}
 	else
+    {
 		return (0);
+    }
 }
