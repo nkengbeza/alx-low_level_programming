7cdbf3c22ba671536b3756593eb62a2b1a478a64
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-14 22:08:32 +0100

    Added 4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 
 /**
   * _islower - Prints 0 if is lower other 1
- * @c: parameter of type char
+ * @c: parameter of type int
   *
   * Description: This application checks for lowercase character.
   *
-  * Return: 0 if successful
+  * Return: 1 or 0
   */
 int _islower(int c)
 {
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _islower - Prints 0 if is lower other 1
+  * @c: parameter of type int
+  *
+  * Description: This application checks if the input
+  * is an alphabetic character
+  *
+  * Return: 1 if is alpha, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+
+	else
+		return (0);
+}
