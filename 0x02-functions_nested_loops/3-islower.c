7fa1af4c2d26021fc34ca3aa9a9076e57c5b5a29
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-14 22:03:46 +0100

    Added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _islower - Prints 0 if is lower other 1
+ * @c: parameter of type char
+  *
+  * Description: This application checks for lowercase character.
+  *
+  * Return: 0 if successful
+  */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
