17e7004004ce77e189ab70059e8d142b1ab19761
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-14 22:24:50 +0100

    Added 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
-  * _islower - Prints 0 if is lower other 1
-  * @c: parameter of type int
-  *
-  * Description: This application checks if the input
-  * is an alphabetic character
-  *
-  * Return: 1 if is alpha, otherwise 0
-  */
+ * _isalpha - Prints 0 if is lower other 1
+ * @c: parameter of type int
+ *
+ * Description: This application checks if the input
+ * is an alphabetic character
+ *
+ * Return: 1 if is alpha, otherwise 0
+ */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
     {
-		return (0);
+        return (1);
+    }
+    else
+    {
+        return (0);
     }
 }
