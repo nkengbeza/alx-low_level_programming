a4fe9ffd2d3f4061395c06043e674373fcb63439
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-15 22:13:09 +0100

    Added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints number 0 to 9
+ *
+ * Description: The application prints number
+ * from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers()
+{
+	for (int i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
