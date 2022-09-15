1d40fb3dc38017608c68e827a5f1de30d10d9a96
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-15 21:58:10 +0100

    Added 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if a letter is digit
+ * @c: parameter of type int
+ *
+ * Description: This application checks if the input
+ * is digit
+ *
+ * Return: 1 if is a digit, otherwise 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
