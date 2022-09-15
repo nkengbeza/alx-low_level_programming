b0295cc96a2162c515f792963194853189f03812
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-15 22:54:24 +0100

    Added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two numbers
+ * @a: parameter of type int
+ * @b: parameter of type int
+ *
+ * Description: This program multiplies two
+ * numbers a and b and returns the product
+ *
+ * Return: int
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
