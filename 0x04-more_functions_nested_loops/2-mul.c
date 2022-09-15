d8d3997e4dd14cc5a41df51a6e68de63c584d1bf
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-15 22:04:06 +0100

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two numbers
+ * @a: parameter of type int
+ * @b: parameter of type int
+ *
+ * Description: This program multiplies two
+ * numbers a and b and returns the product
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
