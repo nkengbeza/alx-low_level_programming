b28d53c5542dc72314ea5fa92c47e41cd3353c1c
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-20 00:41:10 +0100

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of string
+ * @str: parameter, which is a pointer to str
+ *
+ * Returns void
+ */
+void puts_half(char *str)
+{
+	int start = 0, count = 0;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	if (count % 2 == 0)
+	{
+		start = count / 2;
+	} else {
+		start = (count - 1) / 2;
+	}
+	
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start++]);
+	}
+	
+	_putchar('\n');
+}
