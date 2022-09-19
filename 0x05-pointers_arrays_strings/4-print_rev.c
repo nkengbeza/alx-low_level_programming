45d8acdda80e7613cf88e43faec6738d57353161
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-19 23:14:54 +0100

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in, followed by a new line,
+ * @s: parameter, which is a pointer to str
+ *
+ * Returns void
+ */
+void print_rev(char *s)
+{
+	int pos = 0, count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count > 0)
+	{
+		_putchar(s[--count]);
+	}
+	_putchar('\n');
+}
