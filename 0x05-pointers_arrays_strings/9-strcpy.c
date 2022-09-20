34858c9a51049a12aea609f6f8a934c70297f4e3
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-20 01:08:55 +0100

    Added 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,14 @@ char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
 
-	while (*(src + count) != '\0')
+	while (count >= 0)
 	{
 		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+		{
+			break;
+		}
 		count++;
 	}
-	
 	return (dest);
 }
