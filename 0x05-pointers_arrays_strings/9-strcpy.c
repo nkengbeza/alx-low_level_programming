6dc8a562248a5cb0b68abdfed971cb2174c805d9
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-20 01:06:42 +0100

    Added 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies string
+ * @dest: parameter, which is a pointer to dest
+ * @src: parameter, which is a pointer to src
+ *
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (*(src + count) == '\0')
+	{
+		*(dest + count) = *(src + count);
+		count++;
+	}
+	
+	return (dest);
+}
