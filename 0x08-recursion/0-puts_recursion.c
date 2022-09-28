9d8af22006e20dd4d633bd92b5d15d48479a3485
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-09-28 06:26:49 -0700

    Added 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,9 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*(s + 1) != '\0')
+	if (*(s + 0) != '\0')
 	{
-		_putchar(*(s + 1));
+		_putchar(*(s + 0));
 		_puts_recursion(s + 1);
 	}
 	_putchar('\n');
