cd5c0fc6b672316ef827d023ce609c7273d1648f
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-10-07 01:49:29 +0100

    Added 101-mil

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,41 +9,40 @@
  *
  * Return: char pounter
 */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1, l2, i, j;
+	unsigned int a, b, i, j;
 	char *str;
 
-	/*treat NULL as empty string*/
-	if (s1 == NULL)
+	if (s1 == '\0')
 		s1 = "";
-	if (s2 == NULL)
+	if (s2 == '\0')
 		s2 = "";
 
-	l1 = l2 = 0;
-	/*length of both s1 & s2 respectively*/
-	while (s1[l1] != '\0')
-		l1++;
-	while (s2[l2] != '\0')
-		l2++;
+	a = b = 0;
 
-	/*set n to length of s2*/
-	if (n >= l2)
-		n = l2;
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
 
-	str = (char *) malloc((l1 + n + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
+	if (n >= b)
+		n = b;
 
-	/*add s1 to str*/
+	str = (char *) malloc((a + n + 1) * sizeof(char));
+	if (str == '\0')
+		return ('\0');
+
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
-	/*add s2 n bytes to str*/
 	for (j = 0; j < n && s2[j] != '\0'; j++)
 	{
 		str[i] = s2[j];
 		i++;
 	}
+
 	str[i] = '\0';
+
 	return (str);
 }
