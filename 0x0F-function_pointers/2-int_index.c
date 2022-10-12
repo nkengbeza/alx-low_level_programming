335e2594f73b09ca56a8db54186eff9fd75012a2
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-10-12 17:35:07 +0100

    Added 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on each array loop
+ * @array: array of int pointer
+ * @size: int.
+ * @cmp: function pointer.
+ *
+ * Return: int.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(i))
+			{
+				return (i);
+			}
+		}
+	}
+
+	return (-1);
+}
+
