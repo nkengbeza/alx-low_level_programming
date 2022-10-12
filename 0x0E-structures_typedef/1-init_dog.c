7a5e2ad3669be0ea8e969d79fef88362f62ddbdc
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-10-12 01:17:36 +0100

    Added 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,11 +3,11 @@
 /**
  * init_dog - initializes struct dog.
  * @d: struct dog.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: owner of the dog.
+ * @name: char pointer.
+ * @age: float.
+ * @owner: char pointer.
  *
- * Return: no return.
+ * Return: void.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
