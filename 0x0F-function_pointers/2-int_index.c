aa92727d2176bdbecc06a4fe27e98acd384b661a
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-10-12 17:46:02 +0100

    Added 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,14 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		if (size <= 0)
-		{
-			return (-1);
-		}
-
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(i))
+			if (cmp(array[i]))
 			{
 				return (i);
 			}
