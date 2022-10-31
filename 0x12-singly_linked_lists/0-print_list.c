275f2699504e4aba9391ea2768795bec408fa75e
Nkengbeza Clinton <nkengbeza@yandex.ru>
2022-10-31 14:01:25 +0100

    Added 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h";
+
+/**
+ * print_list - prints all the elements
+ * @h: singly linked list.
+ *
+ * Return: number of elements in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num_of_elem = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		num_of_elem++;
+	}
+	return (num_of_elem);
+}
